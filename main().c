4edf39de91ee2de94913f7006005d0a1ad0f2945
ReX342 <occ.rex@gmail.com>
2021-09-14 00:53:45 +0200

    debugerrors

--- main().c
+++ main().c
@@ -2,6 +2,6 @@
 #include <stdint.h>
 int main () {
     uint64_t dx= 0x152345670;
-    while (dx) putchar (0x0417765736F6d65210A >> ((dx >> 4) << 3));
+    while (dx) putchar (0x0417765736F6d65210A >> ((dx >>= 4) << 3));
     return 0;
 }
