2bce7996183c5d7e6768f9b1cf9e13c9849c5d21
ReX342 <occ.rex@gmail.com>
2021-09-14 12:39:47 +0200

    space2

--- main().c
+++ main().c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 int main () {
-    uint64_t dx= 0x152345670;
-    while (dx) putchar (0x0417765736F6d65210A >> ((dx >>= 4) << 3));
+    uint64_t dx = 0x152345670;
+    while (dx) putchar(0x417765736F6d65210A >> ((dx >>= 4) << 3));
     return 0;
 }
